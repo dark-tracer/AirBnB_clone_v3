b2dd75fb23e4ff895753aabdaa181648b27ab80c
dark-tracer <bernieamponsah2@gmail.com>
2024-05-28 22:25:45 -0700

    0x05. AirBnB clone - RESTful API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Flask server (variable app)
-"""
+'''Flask server (variable app)'''
 
-
-from flask import Flask, jsonify
+from flask import Flask
 from models import storage
 from os import getenv
 from api.v1.views import app_views
+from flask import jsonify
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -22,14 +22,16 @@ def downtear(self):
 @app.errorhandler(404)
 def page_not_found(error):
     '''return render_template'''
-    return jsonify('error='Not found'), 404
+    return jsonify({'error': 'Not found'}), 404
 
 
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST')
-    port = getenv('HBNB_API_PORT')
-    if not host:
+    if getenv('HBNB_API_HOST'):
+        host = getenv('HBNB_API_HOST')
+    else:
         host = '0.0.0.0'
-    if not port:
-        port = '5000'
+    if getenv('HBNB_API_PORT'):
+        port = getenv('HBNB_API_PORT')
+    else:
+        port = 5000
     app.run(host=host, port=port, threaded=True)
